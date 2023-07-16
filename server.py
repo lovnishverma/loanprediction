6daab3eeae35697430e1d277e9c588567742e39d
Glitch (glitch-hello-website) <none>
2023-07-16 11:35:40 +0000

    🤗💝 Checkpoint ./loan.csv:57629255/22 ./server.py:57629255/11986 ./requirements.txt:57629255/16

--- server.py
+++ server.py
@@ -1,16 +1,13 @@
-from flask import Flask, render_template, request
 import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import LabelEncoder
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('loan.html')
-
-@app.route('/predict', methods=['POST'])
+@app.route('/', methods=['POST'])
 def predict():
+    # Get the input values from the form
     gender = int(request.form.get('gender'))
     married = int(request.form.get('married'))
     education = int(request.form.get('education'))
@@ -22,28 +19,21 @@ def predict():
     df = pd.read_csv('loan.csv')
 
     # Prepare the data
-    X = df[['Gender', 'Married', 'Education', 'Self_Employed', 'Credit_History', 'Property_Area']]
-    y = df['Loan_Status']
-
-    # Perform label encoding on categorical features
-    label_encoder = LabelEncoder()
-    X_encoded = X.apply(label_encoder.fit_transform)
+    X = df[['gender', 'married', 'education', 'self_employed', 'credit_history', 'property_area']]
+    y = df['loan_status']
 
     # Create a random forest classifier model
     model = RandomForestClassifier()
 
     # Train the model on the entire dataset
-    model.fit(X_encoded, y)
-
-    # Prepare the input data for prediction
-    input_data = pd.DataFrame([[gender, married, education, self_employed, credit_history, property_area]],
-                              columns=['Gender', 'Married', 'Education', 'Self_Employed', 'Credit_History', 'Property_Area'])
-    input_encoded = input_data.apply(label_encoder.transform)
+    model.fit(X, y)
 
     # Make prediction on new data
-    prediction = model.predict(input_encoded)[0]
+    new_data = [[gender, married, education, self_employed, credit_history, property_area]]
+    prediction = int(model.predict(new_data)[0])
 
     return render_template('loan.html', prediction=prediction)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
