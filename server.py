f7abf709a07689869f674ae8716fcdee143a38a2
Glitch (glitch-hello-website) <none>
2023-07-16 10:50:00 +0000

    🚈🏕 Checkpoint ./loan.csv:57629255/37397 ./server.py:57629255/2878 ./templates/loan.html:57629255/8299

--- server.py
+++ server.py
@@ -10,19 +10,25 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    gender = int(request.form.get('gender'))
-    married = int(request.form.get('married'))
-    education = int(request.form.get('education'))
-    self_employed = int(request.form.get('self_employed'))
-    credit_history = int(request.form.get('credit_history'))
-    property_area = int(request.form.get('property_area'))
+    gender = request.form.get('gender')
+    married = request.form.get('married')
+    dependents = request.form.get('dependents')
+    education = request.form.get('education')
+    self_employed = request.form.get('self_employed')
+    applicant_income = int(request.form.get('applicant_income'))
+    coapplicant_income = int(request.form.get('coapplicant_income'))
+    loan_amount = request.form.get('loan_amount')
+    loan_amount_term = request.form.get('loan_amount_term')
+    credit_history = request.form.get('credit_history')
+    property_area = request.form.get('property_area')
 
     # Load the loan dataset
     df = pd.read_csv('loan.csv')
 
     # Prepare the data
-    X = df[['gender', 'married', 'education', 'self_employed', 'credit_history', 'property_area']]
-    y = df['loan_status']
+    X = df[['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'ApplicantIncome',
+            'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History', 'Property_Area']]
+    y = df['Loan_Status']
 
     # Create a logistic regression model
     model = LogisticRegression()
@@ -31,7 +37,8 @@ def predict():
     model.fit(X, y)
 
     # Make prediction on new data
-    new_data = [[gender, married, education, self_employed, credit_history, property_area]]
+    new_data = [[gender, married, dependents, education, self_employed, applicant_income,
+                 coapplicant_income, loan_amount, loan_amount_term, credit_history, property_area]]
     prediction = model.predict(new_data)[0]
 
     return render_template('loan.html', prediction=prediction)
