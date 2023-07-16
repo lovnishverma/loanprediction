49541b14fc7aebf77166369d355f99912fa44d46
Glitch (glitch-hello-website) <none>
2023-07-16 11:24:34 +0000

    ⏲🍁 Checkpoint ./NewFilee.csv:57629255/8676 ./server.py:57629255/10224 ./templates/loan.html:57629255/5920

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
 
 app = Flask(__name__)
 
@@ -12,44 +11,34 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    gender = request.form.get('gender')
-    married = request.form.get('married')
-    dependents = request.form.get('dependents')
-    education = request.form.get('education')
-    self_employed = request.form.get('self_employed')
-    applicant_income = int(request.form.get('applicant_income'))
-    coapplicant_income = int(request.form.get('coapplicant_income'))
-    loan_amount = int(request.form.get('loan_amount'))
-    loan_amount_term = int(request.form.get('loan_amount_term'))
+    gender = int(request.form.get('gender'))
+    married = int(request.form.get('married'))
+    education = int(request.form.get('education'))
+    self_employed = int(request.form.get('self_employed'))
     credit_history = int(request.form.get('credit_history'))
-    property_area = request.form.get('property_area')
+    property_area = int(request.form.get('property_area'))
 
     # Load the loan dataset
     df = pd.read_csv('loan.csv')
 
     # Prepare the data
-    X = df[['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'ApplicantIncome',
-            'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History', 'Property_Area']]
+    X = df[['Gender', 'Married', 'Education', 'Self_Employed', 'Credit_History', 'Property_Area']]
     y = df['Loan_Status']
 
-    # Perform one-hot encoding on categorical features
-    enc = OneHotEncoder(handle_unknown='ignore')
-    X_encoded = enc.fit_transform(X)
-
-    # Handle missing values
-    imputer = SimpleImputer(strategy='mean')
-    X_encoded_filled = imputer.fit_transform(X_encoded)
+    # Perform label encoding on categorical features
+    label_encoder = LabelEncoder()
+    X_encoded = X.apply(label_encoder.fit_transform)
 
     # Create a random forest classifier model
     model = RandomForestClassifier()
 
     # Train the model on the entire dataset
-    model.fit(X_encoded_filled, y)
+    model.fit(X_encoded, y)
 
     # Prepare the input data for prediction
-    input_data = [[gender, married, dependents, education, self_employed, applicant_income,
-                   coapplicant_income, loan_amount, loan_amount_term, credit_history, property_area]]
-    input_encoded = enc.transform(input_data)
+    input_data = pd.DataFrame([[gender, married, education, self_employed, credit_history, property_area]],
+                              columns=['Gender', 'Married', 'Education', 'Self_Employed', 'Credit_History', 'Property_Area'])
+    input_encoded = input_data.apply(label_encoder.transform)
 
     # Make prediction on new data
     prediction = model.predict(input_encoded)[0]
